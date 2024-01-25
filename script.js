d337b711b9f1c9c9b344f1c40673769b04266764
Ayron Matos <mrayronmatos01@gmail.com>
2024-01-25 14:24:33 -0300

    add counter

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const counter = document.querySelector('strong')
+const seconds = document.querySelector('span')
+
 function generateRandomNumber() {
     const randomNumber = Math.floor(Math.random() * 999999) + 1
     const paddedNumber = randomNumber.toString().padStart(6, '0')
@@ -5,4 +8,14 @@ function generateRandomNumber() {
     randomNumberElement.textContent = paddedNumber
 }
 
+function changeCountNumber() {
+    let num = parseInt(counter.textContent - 1)
+    if(num === 0) num = 10
+    counter.textContent = num
+
+    console.log(num)
+    num === 1 ? seconds.textContent = "segundo" : seconds.textContent = "segundos"
+}
+
 setInterval(generateRandomNumber, 10 * 1000)
+setInterval(changeCountNumber, 1 * 1000)
